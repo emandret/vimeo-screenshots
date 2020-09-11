154ba5c221b1d78092b72147e8be079a318c43af
Edwy Mandret <emandret@student.42.fr>
2020-09-11 03:19:57 +0200

    revert for increment

--- browser.js
+++ browser.js
@@ -48,7 +48,7 @@ async function fetchVideoData(id) {
             fs.mkdirSync(imgPath);
         }
 
-        for (let t = 0; t < video.duration;) {
+        for (let t = 0; t < video.duration; t += interval) {
             const page = await browser.newPage();
 
             // Set the viewport width and height
@@ -66,7 +66,6 @@ async function fetchVideoData(id) {
             // Compute delta time
             const diff = process.hrtime(start);
             t += (diff[0] + diff[1] * 1e-9);
-            t += interval;
 
             // Convert the number of seconds to hh:mm:ss
             const date = new Date(Math.round(t) * 1e3).toISOString().substr(11, 8).replace(/\:/g, '');
