1128db7a1f58e4d2d55968d6dd82e3c816a7d808
Edwy MANDRET <emandret@student.42.fr>
2023-03-05 14:00:45 +0000

    removed await expressions in for loop

--- browser.js
+++ browser.js
@@ -1,102 +1,86 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const process = require('process');
-const fetch = require('node-fetch');
-const puppeteer = require('puppeteer-core');
+const fs = require("fs");
+const process = require("process");
+const fetch = require("node-fetch");
+const puppeteer = require("puppeteer-core");
 
 async function fetchVideoData(id) {
-    try {
-        const response = await fetch(`https://vimeo.com/api/v2/video/${id}/json`);
-
-        if (response.status === 200) {
-            return (await response.json())[0];
-        } else {
-            throw new Error(response.status.toString());
-        }
-
-    } catch (e) {
-        console.warn(e);
+  try {
+    const response = await fetch(`https://vimeo.com/api/v2/video/${id}/json`);
+    if (response.status === 200) {
+      return (await response.json())[0];
+    } else {
+      throw new Error(response.status.toString());
     }
+  } catch (e) {
+    console.warn(e);
+  }
 
-    return {};
+  return {}; // Error
+}
+
+async function openPageAndScreenshot(viewportWidth, viewportHeight, pageUrl, filePath) {
+  const page = await browser.newPage();
+  await page.setViewport({ width: viewportWidth, height: viewportHeight });
+  await page.goto(pageUrl, { waitUntil: "networkidle0", timeout: 0 }); // TODO: Change the player file with the bundled Vimeo SDK to tweak the progress in milliseconds
+  await page.screenshot({
+    path: filePath,
+    type: "jpeg",
+    quality: 100,
+    clip: { x: 0, y: 0, width: viewportWidth, height: viewportHeight },
+  });
+  await page.close();
 }
 
 (async () => {
-    const args = process.argv.slice(2);
+  const args = process.argv.slice(2);
 
-    if (args.length < 2) {
-        console.warn('Please specify a valid Vimeo video ID and a time interval!');
-        process.exit();
+  if (args.length < 2) {
+    console.warn("Please specify a valid Vimeo video ID and a time interval!");
+    process.exit();
+  }
+
+  const id = parseInt(args[0]);
+  const interval = parseInt(args[1]);
+
+  if (interval < 1) {
+    console.warn("The time interval must be greater than zero!");
+    process.exit();
+  }
+
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+      executablePath:
+        "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+      ignoreDefaultArgs: ["--disable-dev-shm-usage"],
+    });
+    const video = await fetchVideoData(id);
+
+    const cwd = process.cwd();
+    const saveDir = `${cwd}/video_${id}`;
+
+    // Create the folder if not existing
+    if (!fs.existsSync(saveDir)) {
+      fs.mkdirSync(saveDir);
     }
 
-    const id = parseInt(args[0]);
-    const interval = parseInt(args[1]);
+    // Array of promises
+    const screenshots = [];
+    for (let t = 0; t < video.duration; t += interval) {
+      const url = `file://${cwd}/player.html#v=${id}&t=${Math.round(t)}`;
+      const date = new Date(Math.round(t) * 1e3).toISOString().substr(11, 8).replace(/\:/g, "");
+      const filePath = `${saveDir}/img_${date}.jpeg`;
 
-    if (interval < 1) {
-        console.warn('The time interval must be greater than zero!');
-        process.exit();
+      // All the asynchronous operations are immediately started
+      screenshots.push(openPageAndScreenshot(video.width, video.height, url, filePath));
     }
+    // Wait for all promises to resolve
+    await Promise.all(screenshots);
 
-    // TODO: Remove await expressions inside the for loop (https://eslint.org/docs/rules/no-await-in-loop)
-
-    try {
-        const browser = await puppeteer.launch({
-            headless: true,
-            executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-            ignoreDefaultArgs: ['--disable-dev-shm-usage']
-        })
-
-        const video = await fetchVideoData(id);
-
-        // Current working directory
-        const cwd = process.cwd();
-        const imgPath = `${cwd}/video_${id}`;
-
-        // Create the folder if not existing
-        if (!fs.existsSync(imgPath)) {
-            fs.mkdirSync(imgPath);
-        }
-
-        for (let t = 0; t < video.duration; t += interval) {
-            const page = await browser.newPage();
-
-            // Set the viewport width and height
-            await page.setViewport({ width: video.width, height: video.height });
-
-            // TODO: Change the player file with the bundled Vimeo SDK to tweak the progress in milliseconds
-
-            // Open the player
-            await page.goto(`file://${cwd}/player.html#v=${id}&t=${Math.round(t)}`, { waitUntil: 'networkidle0', timeout: 0 });
-
-            // Start time counter
-            const start = process.hrtime();
-
-            // Screenshot the viewport
-            await page.screenshot({ path: `${imgPath}/x.jpeg`, type: 'jpeg', quality: 100, clip: { x: 0, y: 0, width: video.width, height: video.height } });
-
-            // Compute delta time
-            const diff = process.hrtime(start);
-            t += (diff[0] + diff[1] * 1e-9);
-
-            // Convert the number of seconds to hh:mm:ss
-            const date = new Date(Math.round(t) * 1e3).toISOString().substr(11, 8).replace(/\:/g, '');
-
-            // Rename the screenshot
-            fs.rename(`${imgPath}/x.jpeg`, `${imgPath}/img_${date}.jpeg`, (e) => {
-                if (e) {
-                    console.warn(e);
-                } else {
-                    console.log(`Saved img_${date}.png!`);
-                }
-            });
-
-            await page.close();
-        }
-
-        await browser.close();
-    }
-    catch (e) {
-        console.warn(e);
-    }
+    await browser.close();
+  } catch (e) {
+    console.warn(e);
+  }
 })();
